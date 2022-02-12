ccdba657879281ee90b71f671cb575736d84f947
del <rodelcdavid@gmail.com>
2022-02-12 20:30:37 +0800

    finished temporary showdown pair data

--- src/components/FindAndMatch.js
+++ src/components/FindAndMatch.js
@@ -1,6 +1,16 @@
 import React, { useEffect, useState } from "react";
 
 const FindAndMatch = () => {
+  // const showdownPairList = [];
+
+  const [showdownPairList, setShowdownPairList] = useState([]);
+
+  //use Promise.all to fetch all bookId
+  //get book.selflink
+
+  //use Promise.all to fetch all movieId
+  //  const movieFetchUrl = `https://api.themoviedb.org/3/movie/671?api_key=fbdfc6969e8fbe78212c30a2dcc64f58&language=en-US`
+
   const bookApiKey = "AIzaSyCLSRspHMHw3eFivj1CJHzTksCULHUtMRg";
   //   const movieApiKey = "k_vz1az5ol";
   const movieApiKey = "fbdfc6969e8fbe78212c30a2dcc64f58";
@@ -15,26 +25,45 @@ const FindAndMatch = () => {
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   const handleSearchBook = () => {
-    //fetch book search api
     fetch(
       `https://www.googleapis.com/books/v1/volumes?q=${inputBook}&key=${bookApiKey}`
     )
       .then((res) => res.json())
       .then((data) => setBookResults(data));
-    //then setResults
   };
 
   const handleSearchMovie = () => {
-    //fetch movie search api
     fetch(
       `https://api.themoviedb.org/3/search/movie?api_key=${movieApiKey}&query=${inputMovie}`
     )
       .then((res) => res.json())
       .then((data) => setMovieResults(data));
-    //then setResults
   };
 
-  //   useEffect(() => {}, [bookResults]);
+  const handleAddToShowdown = () => {
+    //What about for movies that have multiple parts??
+
+    const newPair = {
+      id: showdownPairList.length + 1,
+      bookInfo: selectedBook,
+      movieInfo: selectedMovie,
+      bookVotes: 0,
+      movieVotes: 0,
+    };
+
+    setShowdownPairList((prev) => [...prev, newPair]);
+  };
+
+  const handleVoteForBook = (pairId) => {
+    //update bookVotes where id = pairId
+  };
+  const handleVoteForMovie = (pairId) => {
+    //update bookVotes where id = pairId
+  };
+
+  useEffect(() => {
+    console.log(showdownPairList);
+  }, [showdownPairList]);
 
   return (
     <div style={{ padding: "1rem" }}>
@@ -65,10 +94,10 @@ const FindAndMatch = () => {
                     width: "200px",
                   }}
                   key={book.id}
-                  onClick={() => setSelectedBook(book.volumeInfo.title)}
+                  onClick={() => setSelectedBook(book)}
                 >
                   <img
-                    src={book.volumeInfo.imageLinks.thumbnail}
+                    src={book.volumeInfo.imageLinks?.thumbnail}
                     alt=""
                     width="100"
                   />
@@ -95,7 +124,7 @@ const FindAndMatch = () => {
                     width: "200px",
                   }}
                   key={movie.id}
-                  onClick={() => setSelectedMovie(movie.title)}
+                  onClick={() => setSelectedMovie(movie)}
                 >
                   <img
                     src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
@@ -110,10 +139,58 @@ const FindAndMatch = () => {
       </div>
       <div>
         <h3>Selected</h3>
-        <p>Book: {selectedBook ? selectedBook : ""}</p>
-        <p>Movie: {selectedMovie ? selectedMovie : ""}</p>
-        <button>Add to Showdown</button>
+        <p>Book: {selectedBook ? selectedBook.volumeInfo.title : ""}</p>
+        <p>Movie: {selectedMovie ? selectedMovie.title : ""}</p>
+        <button onClick={handleAddToShowdown}>Add to Showdown</button>
       </div>
+
+      {/* required data: pairId, bookInfo, movieInfo, bookVotes, movieVotes */}
+      <div>Temporary Showdown Display</div>
+      {showdownPairList.length &&
+        showdownPairList.map((pair) => {
+          return (
+            <div
+              style={{
+                padding: "1rem",
+                border: "1px solid #aaa",
+                width: "800px",
+                display: "flex",
+                gap: "30px",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              key={pair.id}
+            >
+              {/* Book */}
+              <div
+                style={{ width: "200px", textAlign: "center" }}
+                onClick={() => handleVoteForBook(pair.id)}
+              >
+                <img
+                  src={pair.bookInfo.volumeInfo.imageLinks?.thumbnail}
+                  alt=""
+                  width="100"
+                />
+                <p>{pair.bookInfo.volumeInfo.title}</p>
+              </div>
+
+              <h3>VS</h3>
+
+              {/* Movie */}
+              <div
+                style={{ width: "200px", textAlign: "center" }}
+                onClick={() => handleVoteForMovie(pair.id)}
+              >
+                <img
+                  src={`https://image.tmdb.org/t/p/original/${pair.movieInfo.poster_path}`}
+                  alt=""
+                  width="100"
+                />
+                <p>{pair.movieInfo.title}</p>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 };
